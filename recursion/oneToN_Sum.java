719d3a93b7183bb211c8012d0d3a61785e460b1d
Rahiway <rahiwayco@gmail.com>
2026-06-26 12:04:15 +0530

    1ton

--- recursion/oneToN_Sum.java
+++ recursion/oneToN_Sum.java
@@ -1,3 +1,19 @@
-public class oneToN_Sum {
-    
+import java.util.Scanner;
+
+public class oneToN_Sum{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+        int sum = Sum(n,0);
+        System.out.println("Sum of numbers from 1 to " + n + " is: " + sum);
+        sc.close();
+    }
+
+    public static int Sum(int i,int sum) {
+        if (i<1) {
+            return 0;
+        }
+        return i + Sum(i - 1, sum + i);
+    }
 }
