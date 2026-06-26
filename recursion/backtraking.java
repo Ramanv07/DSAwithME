ddcfd296de82249db7acd17a1f12179305555a79
Rahiway <rahiwayco@gmail.com>
2026-06-26 11:50:22 +0530

    changes

--- recursion/backtraking.java
+++ recursion/backtraking.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class backtraking{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of times to print your name: ");
+        System.out.print("Enter the number : ");
         int n = sc.nextInt();
         namePrint(n);
         sc.close();
@@ -15,8 +15,8 @@ public class backtraking{
         if (n < 1) {
             return;
         }
-        //main logic is that first it will go to the base case and then it will print the name in reverse order
-        namePrint(n - 1);// explaination is that first it will go to the base case and then it will print the name in reverse order
+        //main logic is that first it will go to the base case and then it will print the  in reverse order
+        namePrint(n - 1);// explaination is that first it will go to the base case and then it will print in reverse order
         System.out.println(n);
     }
 }
---
+++ recursion/oneToN_Sum.java
@@ -0,0 +1,3 @@
+public class oneToN_Sum {
+    
+}
